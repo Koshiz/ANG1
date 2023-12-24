abff99d60992460e504872d54973729a3e99bfdc
Koshiz <koshilasandaru2@gmail.com>
2023-12-24 18:57:27 +0530

    Server Component Changes

--- src/app/server/server.component.ts
+++ src/app/server/server.component.ts
@@ -11,8 +11,16 @@ export class AppServerComponent{
     serverId: number = 10;
     serverStatus: string = 'offline';
 
+    constructor(){
+        this.serverStatus = Math.random() > 0.5 ? 'online' : 'offline';
+    }
+
     getServerStatus(){
         return this.serverStatus = 'online';
+    }
+
+    getColor(){
+        return this.serverStatus === 'online' ? 'green' : 'red';
     }
 
 }
